e0cbe621babd5adec153899911c0128f7e2879d2
Sushil Subedi <sushilsubedi32@gmail.com>
2023-12-11 04:20:08 +0545

    user agent change

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -16,8 +16,7 @@ export async function GET(req: NextRequest) {
       `https://www.instagram.com/api/v1/users/web_profile_info/?username=${username}`,
       {
         headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)",
+          "User-Agent": "Instagram 72.0.0.21.98 Android",
         },
       }
     )
